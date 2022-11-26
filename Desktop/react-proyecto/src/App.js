0090f9e4a09d17470bd337be0b4806295f309939
MarcosAuqui <marcos.auqui12345@gmail.com>
2022-11-26 20:16:35 -0300

    inicio

---
+++ Desktop/react-proyecto/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Nav } from './components/navBar/Nav'	
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/react-proyecto/src/components/navBar/Nav.jsx
@@ -0,0 +1,9 @@
+
+
+export const Nav = () => {
+    return (
+        <div>
+            <h1>Esto es el nav</h1>
+        </div>
+    )
+}
